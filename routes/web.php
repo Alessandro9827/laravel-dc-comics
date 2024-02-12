06ec2524c13560a63cbed5a9ddc9373f49f2fb15
Alessandro9827 <ale.moramarco27@gmail.com>
2024-02-12 18:39:16 +0100

    added route web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
+    $comics = config('db.comic');
+    return view('pages.comics', [ 'comics' => $comics]);
 });
+
