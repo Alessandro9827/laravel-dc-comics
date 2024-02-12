f9dc0bf7f02dc1d88a82f59a9796dec61317c4b3
Alessandro9827 <ale.moramarco27@gmail.com>
2024-02-12 19:22:20 +0100

    created seeder, model and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     $comics = config('db.comic');
     return view('pages.comics', [ 'comics' => $comics]);
 });
+*/
+Route::get('/', function(){
+    return view('pages.home');
+});
+
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 
