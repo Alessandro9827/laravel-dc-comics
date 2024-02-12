7a050eeed4a6bbc00ba7050f57312951f0a05b67
Alessandro9827 <ale.moramarco27@gmail.com>
2024-02-12 18:30:11 +0100

    added comics.blade and images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        
-        <title>Model Controller</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>DC Comics</title>
 
-        @vite('resources/js/app.js')
+    @vite('resources/js/app.js')
 
-        @yield('head-content')
-    </head>
-    <body>
-        <!-- Header -->
-        @include('partials.header')
+    @yield('head-content')
+</head>
+<body>
+    
+    @include('partials.header')
 
-        <main>
-            @yield('main-content')
-        </main>
+    <main>
+        @yield('main-content')
+    </main>
 
-        <!-- Footer -->
-        @include('partials.footer')
+    @include('partials.footer')
 
-        @yield('script-content')
-    </body>
+    @yield('script-content')
+</body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,26 @@
 <header>
-    <h1>Header</h1>
+    <section class="container d-flex justify-content-between align-items-center">
+        <section class="logo">
+            <img src="{{ Vite::asset('resources/assets/images/dc-logo.png') }}" alt="DC comics main logo">
+        </section>
+        <section class="navigation">
+            <nav class="navbar-nav navbar-light">
+                <ul class="d-flex gap-5 text-uppercase list-unstyled m-0">
+                    <li>Characters</li>
+                    <li class="active">Comics</li>
+                    <li>Movies</li>
+                    <li>Tv</li>
+                    <li>Games</li>
+                    <li>Collectibles</li>
+                    <li>Videos</li>
+                    <li>Fans</li>
+                    <li>News</li>
+                    <li>Shop</li>
+                </ul>
+            </nav>
+        </section>
+    </section>
+    <section class="jumbotron">
+        <img src="{{Vite::asset('resources/assets/images/jumbotron.jpg')}}" alt="">
+    </section>
 </header>
